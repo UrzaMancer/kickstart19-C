fa0ecfd0df6363758776f3b9948d67df04562a9b
jechamt <dev@admin01.net>
2019-05-26 02:52:57 -0700

    dogsToObserver not needed to bucketColors

--- CatchSome.cpp
+++ CatchSome.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define TESTSETFILE "template.txt"
 
 const bool debug = true;
-vector<int> bucketColors(vector<int> dColors, int dogCount) {
+vector<int> bucketColors(vector<int> dColors) {
     int largestColor = 0;
     vector<int> buckets(1000, 0); //There can only be 1000 colors: 1 <= A(i) <= 1000
     for(int i = 0; i < dColors.size(); ++i) {
@@ -37,7 +37,7 @@ int runTestCase(ifstream& inFile) {
         inFile >> buffer;
         dogColors.push_back(buffer);
     }
-    bucketColors(dogColors, dogsToObserve);
+    bucketColors(dogColors);
     return;
 }
 
