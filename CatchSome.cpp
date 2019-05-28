11839ea76470fce375d493e36e70d37c7d280f96
jechhamt <dev@admin01.net>
2019-05-27 18:48:28 -0700

    Added Observer class to retain state of Bundle, timeTaken, dogsLeft, and current position and shirt color

--- CatchSome.cpp
+++ CatchSome.cpp
@@ -8,7 +8,88 @@
 using namespace std;
 #define TESTSETFILE "template.txt"
 
+/*
+Problem
+Bundle is an animal researcher and needs to go observe K dogs. She lives on a horizontal street marked at metre increments with consecutive numbers 0, 1, 2, 3 and so on. She begins in her home, which is at position 0. There are also N dogs on the street. The i-th dog is Pi metres to the right of her home on the street (multiple dogs can share the same position).
+
+Dogs come in different colors, which are denoted by positive integers. The i-th animal is of color Ai.
+
+If Bundle is at her home, she can change the current color of her shirt. This is important since the dogs are very shy! Bundle can only observe a dog if she is at the same position as that dog, and is wearing a shirt of the same color as the dog.
+
+It takes Bundle one second to move one metre to the left or right on the street. It takes her no time to change shirts or observe a dog.
+
+What is the least amount of time it will take Bundle to observe K dogs? Note that she does not have to return home after observing K dogs.
+Input
+
+The first line of the input gives the number of test cases, T. T test cases follow. Each testcase begins with a line containing the two integers N and K, the number of dogs on the number line and the number of dogs Bundle needs to observe, respectively. The second line contains N integers, the i-th of which is Pi, the position of the i-th dog. The third line contains N integers, the i-th of which is Ai, the color of the i-th dog.
+Output
+
+For each test case, output one line containing Case #x: y, where x is the test case number (starting from 1) and y is the least time Bundle needs to observe K dogs.
+Limits
+
+Time limit: 30 seconds per test set.
+Memory limit: 1GB.
+1 ≤ T ≤ 100.
+1 ≤ K ≤ N <= 1000.
+1 ≤ Ai ≤ 1000.
+1 ≤ Pi ≤ 105.
+*/
+
 const bool debug = true;
+
+class Observer {
+    private:
+    int position;
+    int shirtColor;
+    int dogsLeft;
+    int timeTaken;
+
+    public:
+    Observer(int);
+    int getPosition();
+    int getShirtColor();
+    int getRemainingDogCount();
+    int getTimeSoFar();
+    void observeDog(int, int);
+    void changeShirt(int);
+}
+
+Observer::Observer(int dogsToObserve) {
+    position = 0; //new Observers start at home
+    shirtColor = 0; //new Observer has no shirt color
+    dogsLeft = dogsToObserve;
+    timeTaken = 0; //new Observe has not taken any seconds
+}
+
+int Observer::getPosition() {
+    return position;
+}
+
+int Observer::getShirtColor() {
+    return shirtColor;
+}
+
+int Observer::getRemainingDogCount() {
+    return dogsLeft;
+}
+
+int Observer::getTimeSoFar() {
+    return timeTaken;
+}
+
+void Observer::observeDog(int dogPos, int dogColor) {
+    if (shirtColor == dogColor) {
+        timeTaken += abs(dogPos - position);
+        position = dogPos;
+        --dogsLeft;
+    }
+    else {
+        timeTaken += dogPos + position; //Observer has to go home to change shirt, then go to dog
+        shirtColor = dogColor;
+        position = dogPos;
+        --dogsLeft;
+    }
+}
 
 class Dog {
     private:
@@ -61,8 +142,33 @@ int Dog::getObsCost(int bundlePosition, int bundleShirtColor) {
     return buckets;
 }*/
 
-void findAllDogs(Dog& emptyList) {
+void findAllDogs(Dog& empytList, ifstream& inFile) {
+    vector<int> dogPosBuffer;
+    dogPosBuffer.reserve(numberOfDogs);
+    for(int i = 0; i < numberOfDogs; ++i) {
+        inFile >> dogPosBuffer[i];
+    }
+    for(int j = 0; j < numberOfDogs; ++j) {
+        int dogColorBuffer;
+        inFile >> dogColorBuffer;
+        Dog nextDog(dogPosBuffer[j], dogColorBuffer);
+        emptyList.push_back(nextDog);
+    }
+}
 
+void observeNextDog(Dog& remainingDogs, int bundlePosition, int bundleColor) {
+    int bundle
+    for ( int k = 0; k < remainingDogs.size(); ++k ) {
+        remainingDogs.getObsCost()
+    }
+}
+int observeAllDogs(Dog& hiddenDogs, int dogsToObserve) {
+    int bundlePosition = 0;
+    int timeTaken = 0;
+    int bundleColor;
+    while ( dogsToObserve > 0 ) {
+        observeNextDog(hiddenDogs, bundlePosition, 0);
+    }
 }
 int runTestCase(ifstream& inFile) {
     int numberOfDogs, dogsToObserve;
@@ -70,17 +176,8 @@ int runTestCase(ifstream& inFile) {
     inFile >> numberOfDogs >> dogsToObserve;
     vector<Dog> allDogs;
     allDogs.reserve(numberOfDogs);
-    for(int i = 0; i < numberOfDogs; ++i) {
-        int posBuffer, ; //TODO: We get the positions all together first, then we get the colors all together. We need to construct dogs using position, color. How can we do this efficiently?
-        inFile >> buffer;
-        dogPositions.push_back(buffer);
-    }
-    for(int j = 0; j < numberOfDogs; ++j) {
-        int buffer;
-        inFile >> buffer;
-        dogColors.push_back(buffer);
-    }
-    vector<int> buckets = bucketColors(dogColors);
+    findAllDogs(allDogs, inFile);
+    /*vector<int> buckets = bucketColors(dogColors);
     vector<int> descendingColors;
     descendingColors.reserve(dogsToObserve);
     descendingColors.push_back(numberOfDogs + 1);
@@ -108,11 +205,7 @@ int runTestCase(ifstream& inFile) {
     }
     descendingColors.erase(descendingColors.begin());
     int lastPosition = 0;
-    for (int n = 0; n < numberOfDogs; ++n) {
-        
-
-    }
-    return;
+    */
 }
 
 void main() {
